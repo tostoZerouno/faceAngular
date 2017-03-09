ecbf07f5dc0ecbfeda1d43f7970b59a9e3ac6468
tostoZerouno <tommaso.tosi92@outlook.it>
2017-03-09 17:40:43 +0100

    select source

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -15,13 +15,64 @@ export class VideoComponent implements OnInit {
 
   ngAfterViewInit() {
     let _video = this.video.nativeElement;
+    this.enumerate();
+    var videoSource = (<HTMLSelectElement> document.getElementsByName("videoSelect")[0]).value;
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
+      navigator.mediaDevices.getUserMedia({ video: {deviceId: videoSource ? {exact: videoSource} : undefined}})
         .then(stream => {
           _video.src = window.URL.createObjectURL(stream);
+          console.log(_video.src);
           _video.play();
+          
         })
     }
+  }
+
+  enumerate() {
+    navigator.mediaDevices.enumerateDevices()
+      .then(gotDevices)
+      .catch(errorCallback);
+
+    var audioInputSelect = (<HTMLSelectElement> document.getElementsByName("audioInputSelect")[0]);
+    var audioOutputSelect = (<HTMLSelectElement> document.getElementsByName("audioOutputSelect")[0]);
+    var videoSelect = (<HTMLSelectElement> document.getElementsByName("videoSelect")[0]);
+
+    function gotDevices(deviceInfos) {
+      
+
+      for (var i = 0; i !== deviceInfos.length; ++i) {
+        var deviceInfo = deviceInfos[i];
+        var option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'audioinput') {
+          option.text = deviceInfo.label ||
+            'Microphone ' + (audioInputSelect.length + 1);
+          audioInputSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'audiooutput') {
+          option.text = deviceInfo.label || 'Speaker ' +
+            (audioOutputSelect.length + 1);
+          audioOutputSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+          option.text = deviceInfo.label || 'Camera ' +
+            (videoSelect.length + 1);
+          videoSelect.appendChild(option);
+        }
+      }
+
+      //console.log(audioInputSelect);
+      console.log(audioOutputSelect.value);
+      console.log(videoSelect.value);
+      console.log(navigator.mediaDevices.getUserMedia({video:true}));
+
+    }
+
+    function errorCallback(error) {
+      console.log(error);
+
+    }
+
+
+
   }
 
   ngOnInit() {
