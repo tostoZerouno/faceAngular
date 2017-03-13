c4661f7a5d76b4d9e235fef843b11ec6fc661b6f
tostoZerouno <tommaso.tosi92@outlook.it>
2017-03-13 08:54:28 +0100

    risolti bug

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -7,66 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoComponent implements OnInit {
   age = "Clicca sull'immagine per cominciare";
-  description = ["no description"];
+  description = "no description";
+  enableCapture = false;
 
-  //fermaticazzo = 0;
-  fermaticazzo = false;
-  constructor() {
-  }
+  constructor() { }
 
   computerVision(blob) {
 
-    const visionApiUrl = 'https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description';
+    return new Promise((resolve, reject) => {
+      const visionApiUrl = 'https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description';
 
-    let p = this.description;
-    //var formData = new FormData();
-    //formData.append("file", dataURItoBlob(stream));
+      var xhrvision = new XMLHttpRequest();
+      xhrvision.open('POST', visionApiUrl, true);
+      xhrvision.setRequestHeader('content-type', 'application/octet-stream');
+      xhrvision.setRequestHeader('Ocp-Apim-Subscription-Key', "b10fb5b057fe4f9cbeac59dcf0f5727f");
 
-    var xhrvision = new XMLHttpRequest();
-    xhrvision.open('POST', visionApiUrl, true);
-    //xhr.setRequestHeader('content-type', 'image/png');
-    xhrvision.setRequestHeader('content-type', 'application/octet-stream');
-    //xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
-    xhrvision.setRequestHeader('Ocp-Apim-Subscription-Key', "b10fb5b057fe4f9cbeac59dcf0f5727f");
-
-
-    xhrvision.onreadystatechange = function () {//Call a function when the state changes.
-      if (xhrvision.status == 200) {
-
-        var resp = JSON.parse(xhrvision.response);
-        console.log(resp.description.captions[0].text);
-        p[0] = resp.description.captions[0].text;
-
-
-      } else {
-        console.log(xhrvision.status);
+      xhrvision.onreadystatechange = function () {
+        if (xhrvision.status == 200) {
+          var resp = JSON.parse(xhrvision.response);
+          console.log(resp.description.captions[0].text);
+          resolve(resp.description.captions);
+        } else {
+          console.log(xhrvision.status);
+        }
       }
-    }
-    //console.log(this.dataURItoBlob(stream));
-    xhrvision.send(blob);
-
+      xhrvision.send(blob);
+    })
   }
 
   evaluateAge() {
-    this.age = this.fermaticazzo  ? "Cercando...(clicca sull'immagine per mettere in pausa)" : "In pausa(clicca sull'immagine per ricominciare)";
-    if (this.fermaticazzo ) {
-      //console.log(this.fermaticazzo);
+    this.age = this.enableCapture ? "Cercando...(clicca sull'immagine per mettere in pausa)" : "In pausa(clicca sull'immagine per ricominciare)";
+    if (this.enableCapture) {
       const video = <any>document.getElementsByTagName('video')[0];
       const canvas = <any>document.getElementsByName('canvas')[0];
-      //console.log(video.height);
-      //console.log(video.videoHeight);
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
       canvas.getContext('2d').drawImage(video, 0, 0);
-      //console.log(canvas.toDataURL('image/png'));
       var image = canvas.toDataURL('image/jpeg', 0.3);
 
       this.getAgeFromImage(image).then(imageAge => {
-        //console.log(imageAge);
         this.clearCanvas();
         const vCanvas = <any>document.getElementsByName('videoCanvas')[0];
         const ctx = vCanvas.getContext('2d');
-        //ctx.clearRect(0, 0, vCanvas.width, vCanvas.height);
         ctx.strokeStyle = "#FF0000";
         const fs = video.width / 20;
         ctx.font = fs + "px Georgia";
@@ -88,28 +70,23 @@ export class PhotoComponent implements OnInit {
             "sadness": "triste",
             "surprise": "sorpreso"
           }
-          //console.log(element.faceAttributes);
           const rect = element.faceRectangle;
           const left = (rect.left + rect.width) / resize;
           var top = (rect.top /*+ rect.height*/) / resize;
           ctx.strokeRect(rect.left / resize, rect.top / resize, rect.width / resize, rect.height / resize);
-          var text = age + " anni, ";// +
-          //(smile > 0.5 ? "felice" : "triste");
 
+          var text = age + " anni, ";// +
           ctx.fillText(text, left, top);
           top += 1.3 * fs;
 
           var scores = element.scores;
-
           var max = Math.max.apply(null, Object.keys(scores).map(function (x) { return scores[x] }));
           text = (Object.keys(scores).filter(function (x) { return scores[x] == max; })[0]);
-
           ctx.fillText(emotionMapping[text], left, top);
           top += 1.3 * fs;
 
           text = (facialHair.beard >= 0.2 ? "barba " : "") +
             (facialHair.moustache >= 0.2 ? "baffi " : "");
-
           ctx.fillText(text, left, top);
           top += 1.3 * fs;
 
@@ -127,21 +104,17 @@ export class PhotoComponent implements OnInit {
             default:
               text = "";
           }
-
           ctx.fillText(text, left, top);
           //top+=1.3*fs;
-
-
         });
 
         setTimeout(() => this.evaluateAge(), 3000);
-        //this.evaluateAge();
       })
     }
   }
 
   onClick() {
-    this.fermaticazzo = !(this.fermaticazzo);
+    this.enableCapture = !(this.enableCapture);
     this.evaluateAge();
   }
 
@@ -151,11 +124,8 @@ export class PhotoComponent implements OnInit {
     video.width = parent.innerWidth / 2;
     var ratio = video.videoHeight / video.videoWidth;
     video.height = video.width * ratio;
-    //console.log(video.height + " " + parent.innerHeight);
-    //console.log("ratio: " + ratio);
     canvas.height = video.height;
     canvas.width = video.width * 2;
-    //console.log(canvas.height + " " + video.height);
   }
 
   getAgeFromImage(stream) {
@@ -164,7 +134,9 @@ export class PhotoComponent implements OnInit {
     var blob = this.dataURItoBlob(stream);
     var finalresponse;
     var comp = this;
-    this.computerVision(blob);
+    this.computerVision(blob).then(captions => {
+      this.description = captions[0].text;
+    });
     return new Promise(
       (resolve, reject) => {
         const faceApiUrl = [
@@ -178,12 +150,10 @@ export class PhotoComponent implements OnInit {
 
         var xhrface = new XMLHttpRequest();
         xhrface.open('POST', faceApiUrl, true);
-        //xhr.setRequestHeader('content-type', 'image/png');
         xhrface.setRequestHeader('content-type', 'application/octet-stream');
         xhrface.setRequestHeader('Ocp-Apim-Subscription-Key', "6e2715cbea564f4f95f9a097e935e8c7");
 
-
-        xhrface.onreadystatechange = function () {//Call a function when the state changes.
+        xhrface.onreadystatechange = function () {
           if (xhrface.status == 200) {
             var resp = JSON.parse(xhrface.response);
             face = true;
@@ -197,14 +167,12 @@ export class PhotoComponent implements OnInit {
             resolve(xhrface.status);
           }
         }
-        xhrface.send(blob);
+        if (!face) xhrface.send(blob);
 
         var xhremotion = new XMLHttpRequest();
-
         xhremotion.open('POST', emotionApiUrl, true);
         xhremotion.setRequestHeader('content-type', 'application/octet-stream');
         xhremotion.setRequestHeader('Ocp-Apim-Subscription-Key', "81f079954302459e904d8c98d06263b1");
-
         xhremotion.onreadystatechange = function () {
           if (xhremotion.status == 200) {
             var resp = JSON.parse(xhremotion.response);
@@ -219,7 +187,7 @@ export class PhotoComponent implements OnInit {
             resolve(xhremotion.status);
           }
         }
-        xhremotion.send(this.dataURItoBlob(stream));
+        if (!emotion) xhremotion.send(this.dataURItoBlob(stream));
       });
   }
 
@@ -273,23 +241,20 @@ export class PhotoComponent implements OnInit {
     return final;
   }
 
-  videoButtonClick(event){
+  videoButtonClick(event) {
     this.clearCanvas();
     console.log(event);
-    if(event=="stop"){
-      this.fermaticazzo=false;
+    if (event == "stop") {
+      this.enableCapture = false;
     }/*else{
-      this.fermaticazzo=true;
+      this.enableCapture=true;
       this.evaluateAge();
     }*/
-
   }
 
   clearCanvas() {
     const vCanvas = <any>document.getElementsByName('videoCanvas')[0];
     const ctx = vCanvas.getContext('2d');
     ctx.clearRect(0, 0, vCanvas.width, vCanvas.height);
-
   }
-
 }
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -8,31 +8,24 @@ import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/cor
 
 export class VideoComponent implements OnInit {
 
- @Output() stop: EventEmitter<string> = new EventEmitter<string>();
+  @Output() stop: EventEmitter<string> = new EventEmitter<string>();
 
   videoSelect = (<HTMLSelectElement>document.getElementsByName("videoSelect")[0]);
   vid;
   localstream;
   bottone = "Ferma il Video"
 
-  constructor() {
-    //this.enumerate();
-    /*this.videoSelect.onselect = function(){
-      console.log(this);
-    }*/
-  }
+  constructor() { }
 
   selectSource() {
     var videoSelect = (<HTMLSelectElement>document.getElementsByName("videoSelect")[0]);
     var videoSource = videoSelect.value;
-    //console.log(videoSelect);
     this.vid = "" + (videoSelect.value);
     console.log("vid: " + this.vid);
     //var constraints  = { video: { deviceId: { exact: videoSource } } };
     var constraints = { video: { deviceId: videoSource } };
     this.stopStream();
     setTimeout(this.startStream(constraints), 150);
-    //this.ngAfterViewInit();
   }
 
   @ViewChild('video') video: any;
@@ -52,31 +45,25 @@ export class VideoComponent implements OnInit {
     var videoSource = videoSelect.value;
     console.log(videoSource);
     var constraints = { video: { deviceId: { exact: videoSource } } };
-    if (!_video.paused){
-        this.stopStream();
-        this.bottone = "Avvia il Video";
-        this.stop.emit("stop");
-    }else{
+    if (!_video.paused) {
+      this.stopStream();
+      this.bottone = "Avvia il Video";
+      this.stop.emit("stop");
+    } else {
       this.startStream(constraints);
       this.bottone = "Ferma il Video";
       this.stop.emit("start");
     }
-    
-    //this.startStream(constraints);
-    
   }
 
   startStream(constraints) {
     let _video = this.video.nativeElement;
-    //this.enumerate();
-    //var videoSource = (<HTMLSelectElement>document.getElementsByName("videoSelect")[0]).value;
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia(constraints)
         .then(stream => {
           _video.src = window.URL.createObjectURL(stream);
           console.log(_video.src);
           this.localstream = stream;
-          //_video.paused=false;
           _video.load();
 
         })
@@ -94,7 +81,6 @@ export class VideoComponent implements OnInit {
         track.stop();
       });
     }
-
     console.log("Video off");
   }
 
@@ -103,54 +89,29 @@ export class VideoComponent implements OnInit {
     navigator.mediaDevices.enumerateDevices()
       .then(gotDevices)
       .catch(errorCallback);
-
-    /*var audioInputSelect = (<HTMLSelectElement>document.getElementsByName("audioInputSelect")[0]);
-    var audioOutputSelect = (<HTMLSelectElement>document.getElementsByName("audioOutputSelect")[0]);*/
     var videoSelect = (<HTMLSelectElement>document.getElementsByName("videoSelect")[0]);
 
     function gotDevices(deviceInfos) {
-
-
       for (var i = 0; i !== deviceInfos.length; ++i) {
         var deviceInfo = deviceInfos[i];
         var option = document.createElement('option');
         option.value = deviceInfo.deviceId;
-        /*if (deviceInfo.kind === 'audioinput') {
-          option.text = deviceInfo.label ||
-            'Microphone ' + (audioInputSelect.length + 1);
-          audioInputSelect.appendChild(option);
-        } else if (deviceInfo.kind === 'audiooutput') {
-          option.text = deviceInfo.label || 'Speaker ' +
-            (audioOutputSelect.length + 1);
-          audioOutputSelect.appendChild(option);
-        } else */
         if (deviceInfo.kind === 'videoinput') {
           option.text = deviceInfo.label || 'Camera ' +
             (videoSelect.length + 1);
           videoSelect.appendChild(option);
         }
       }
-
-      //console.log(audioInputSelect);
-      /*console.log(audioOutputSelect.value);
-      console.log(videoSelect.value);*/
-      //this.vid = "a: " + videoSelect.value;
-      //console.log(navigator.mediaDevices.getUserMedia({ video: true }));
-
     }
 
     function errorCallback(error) {
       console.log(error);
-
     }
-
-
-
   }
 
   ngOnInit() {
     this.enumerate();
-/*    this.enumerate();*/
+    /*    this.enumerate();*/
   }
 
 }
